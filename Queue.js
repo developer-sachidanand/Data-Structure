c0d332df2e7bd218daec28c578911985a120758b
Sachidanand <74866315+developer-sachidanand@users.noreply.github.com>
2021-01-10 22:07:33 +0530

    Add files via upload
    This is the implementation of the queue data structure using the javascript language within the chrome browser

---
+++ Queue.js
@@ -0,0 +1,42 @@
+class Node{
+	constructor(value){
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue{
+	constructor(){
+		this.first=null;
+		this.last = null;
+		this.size = 0;
+	}
+	
+	Enqueue(val){
+		var newNode = new Node(val);
+		if(!this.first){
+			this.first = newNode;
+			this.last = newNode;
+		}
+		else{
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+		return ++this.size;
+	}
+	
+	Dequeue(){
+		if(!this.first){
+			return null;
+		}
+		var temp = this.first;
+		if(this.first === this.last){
+			this.last = null;
+		}
+		this.first = this.first.next;
+		this.size--;
+		return temp.value;
+	}
+}
+
+var newQueue = new Queue();
