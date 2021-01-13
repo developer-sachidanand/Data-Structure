2aa7c9ea10b1e44a5024562fcb7f62b5f680c371
Sachidanand <74866315+developer-sachidanand@users.noreply.github.com>
2021-01-13 23:13:09 +0530

    Update BinarySearchTrees.js
    Implementation of breadth first search in the binary search tree class

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -61,6 +61,22 @@ class BinarySearchTrees{
 		}
 		return false;
 	}
+	 //implementation of breadth first search
+	BFS(){
+		var node = this.root,
+				   data = [],
+				   queue = [];
+				   
+		queue.push(node);
+		while(queue.length){
+			node = queue.shift();
+			data.push(node.value);
+			if(node.left) queue.push(node.left);
+			if(node.right) queue.push(node.right);
+		}
+		
+		return data;
+	}
 }
 
 
