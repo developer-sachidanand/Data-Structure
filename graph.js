211ea5a427c8ba8940a58351bc71f831d34c4f8c
Sachidanand <74866315+developer-sachidanand@users.noreply.github.com>
2021-01-17 22:05:42 +0530

    Update graph.js
    implemented remove edge and remove vertex method

--- graph.js
+++ graph.js
@@ -13,6 +13,21 @@ class Graph{
         this.adjList[v1].push(v2);
         this.adjList[v2].push(v1);
     }
+    
+    removeEdge(vertex1,vertex2){
+		this.adjList[vertex1] = this.adjList[vertex1].filter( v => v !== vertex2 );
+		
+		this.adjList[vertex2] = this.adjList[vertex2].filter( v => v !== vertex1 );
+	}
+	
+	removeVertex(vertex){
+		while(this.adjList[vertex].length){
+			const adjVertex = this.adjList[vertex].pop();
+			
+			this.removeEdge(vertex,adjVertex);
+		}
+		delete this.adjList[vertex]
+	}
 }
 
 let graph = new Graph();
